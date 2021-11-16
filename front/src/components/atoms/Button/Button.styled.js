85fe110511a532528c4ba483d46f348bd8c69df7
Vitaliy Prachov <mastersasori007@gmail.com>
2021-11-16 17:34:52 +0200

    refactor: use theme variables

--- front/src/components/atoms/Button/Button.styled.js
+++ front/src/components/atoms/Button/Button.styled.js
@@ -1,27 +1,30 @@
 import styled from 'styled-components';
 
+import variables from '@sb-ui/theme/variables';
+
 export const Button = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 0.25rem;
   height: 22px;
-  padding: 1rem 1rem;
+  padding: 1rem;
   cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
   user-select: none;
   ${(props) => (props.bold ? `font-weight: bold;` : '')}
 
-  ${(props) => (props.active ? `background-color: #f0f0f0;` : '')}
+  ${(props) =>
+    props.active ? `background-color: ${variables['gray-4']};` : ''}
   
   ${(props) =>
     props.disabled
-      ? `color: #BFBFBF;`
+      ? `color: ${variables['gray-6']};`
       : `
       &:hover {
-        background-color: #f0f0f0;
+        background-color: ${variables['gray-4']};
       }
       &:active {
-        background-color: #d9d9d9;
+        background-color: ${variables['gray-5']};
       }    
       `}
 `;
--- front/src/theme/variables.js
+++ front/src/theme/variables.js
@@ -23,6 +23,8 @@ const variables = {
   'background-color-light': '#fafafa',
   'editorjs-grey-color': '#707684',
   'gray-4': '#f0f0f0',
+  'gray-5': '#d9d9d9',
+  'gray-6': '#bfbfbf',
 };
 
 module.exports = variables;
