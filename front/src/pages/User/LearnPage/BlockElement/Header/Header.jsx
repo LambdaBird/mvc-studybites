bfc97dd94c94ce114a9a0dc132eab92e547ea679
Tim Reznichenko <tim.reznich@gmail.com>
2021-12-06 15:26:54 +0200

    style: same header tag in teacher and student views

--- front/src/pages/User/LearnPage/BlockElement/Header/Header.jsx
+++ front/src/pages/User/LearnPage/BlockElement/Header/Header.jsx
@@ -1,14 +1,11 @@
-import { Typography } from 'antd';
-
 import { htmlToReact } from '@sb-ui/pages/User/LearnPage/utils';
 
 import { HeaderContentType } from '../types';
 
-const { Title } = Typography;
-
 const Header = ({ content }) => {
   const { text, level } = content.data;
-  return <Title level={level}>{htmlToReact(text)}</Title>;
+  const HeaderTag = `h${level}`;
+  return <HeaderTag>{htmlToReact(text)}</HeaderTag>;
 };
 
 Header.propTypes = {
